40c52a86b1fa87def6ed8b1999b5d2ab7059edab
beckerd <daniel.becker@gdv-dl.de>
2022-11-25 10:59:16 +0100

    Created connection to server - HttpURLConnection

--- app/src/main/java/com/darkvoidstudios/encryptedmessenger/database/LoginController.java
+++ app/src/main/java/com/darkvoidstudios/encryptedmessenger/database/LoginController.java
@@ -3,21 +3,18 @@ package com.darkvoidstudios.encryptedmessenger.database;
 import com.darkvoidstudios.encryptedmessenger.account.User;
 import com.darkvoidstudios.encryptedmessenger.crypto.EncryptService;
 
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginController {
 
     public void loginToAccount(String username, String unhashedPassword) throws IOException, JSONException {
-        String postEndpoint = "https://api.darkvoidstudios.com/messenger/login";
+        URL url = new URL("https://api.darkvoidstudios.com/messenger/login");
 
         //Preparing login data
         User user = new User();
@@ -28,11 +25,38 @@ public class LoginController {
 
         //Creating user in json format
         JSONObject jsonUser = new JSONObject();
+        //jsonUser.put("username", user.getUsername());
+        //jsonUser.put("password", user.getHashedPassword());
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
+        con.setDoInput(true);
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestMethod("POST");
+
         jsonUser.put("username", user.getUsername());
         jsonUser.put("password", user.getHashedPassword());
 
+        OutputStreamWriter wr= new OutputStreamWriter(con.getOutputStream());
+        wr.write(jsonUser.toString());
+
+
+
+
+
         //TODO Sending via POST Method
 
+       /* URL url = new URL("http://www.android.com/");
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+        } finally {
+            urlConnection.disconnect();
+        }
+
+/*
         HttpClient httpClient = HttpClientBuilder.create().build();
 
         HttpPost request = new HttpPost(postEndpoint);
@@ -47,7 +71,7 @@ public class LoginController {
         //Test output
         System.out.println("Hash: " + user.getHashedPassword());
         System.out.println("JSON String: " + jsonUser);
-
+*/
         //TODO TOKEN ABFANGEN
         user.setToken(null);
     }
